db7f3035c68a4d65d93a3d8544babc2db8f89acb
MrBrenlla <bgbrenlla@gmail.com>
2024-03-06 02:10:02 +0100

    Creada actividade gastos grupo e layout listo + arreglos en grupos

---
+++ app/src/main/java/com/example/app/gastos_grupo.kt
@@ -0,0 +1,11 @@
+package com.example.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class gastos_grupo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gastos_grupo)
+    }
+}
