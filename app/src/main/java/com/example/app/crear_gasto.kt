4efbb6d302478d77d83ec51ae00f5a03df18f682
pedro-vales <pedro.vales@udc.es>
2024-03-08 16:57:36 +0100

    Añadida pantalla inicial y primer Intent en gastos.kt

---
+++ app/src/main/java/com/example/app/crear_gasto.kt
@@ -0,0 +1,11 @@
+package com.example.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class crear_gasto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_gasto)
+    }
+}
